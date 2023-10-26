6041edd1d3a3c28408873e997ae8ec7c8341112c
Yohai Wiener <yohaiwiener@gmail.com>
2023-10-26 14:03:34 +0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as LocalAuth from 'expo-local-authentication';
+import { useState } from 'react';
 
 export default function App() {
+  const [authRes, setAuthRes] = useState<LocalAuth.LocalAuthenticationResult | null>(null);
+
+  const callAuth = async () => {
+    const res = await LocalAuth.authenticateAsync();
+    setAuthRes(res);
+    console.log(res);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Authenticate" onPress={callAuth} />
       <StatusBar style="auto" />
     </View>
   );
